b44adbbd38e3d7943fb0129afa1eed4e9acca8cb
Felipe-Sodré <felipe.souldre.com@hotmail.com>
2025-05-07 19:25:31 -0300

    Add arquivos ao projeto

--- MauiAppLogin/App.xaml.cs
+++ MauiAppLogin/App.xaml.cs
@@ -8,5 +8,20 @@
 
             MainPage = new AppShell();
         }
-    }
-}
+    
+    
+        protected override Window CreateWindow(IActivationState ActivationState)
+        {
+
+            var window = base.CreateWindow(ActivationState);
+
+            window.Width = 400;
+            window.Height = 600;
+
+            return window;
+        }
+            
+     
+     }//Fecha Classe
+
+}//Fechar namespace
